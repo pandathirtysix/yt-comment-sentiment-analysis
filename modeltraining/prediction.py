e8a8586c0c8112411f38c77ea8f8d4f04c035570
PANDAG36 <ripwitepubg@gmail.com>
2026-09-06 18:02:30 +0530

    fixed path issue in deploynment

--- modeltraining/prediction.py
+++ modeltraining/prediction.py
@@ -3,7 +3,10 @@ from tensorflow.keras.models import load_model
 import os 
 
 
-model = load_model("models\stacked_bilstm.keras")
+MODEL_PATH = os.path.join(os.getcwd(), "models", "stacked_bilstm.keras")
+
+model = load_model(MODEL_PATH)
+
 
 def model_predict(data):
     try:
